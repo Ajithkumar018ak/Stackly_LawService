b0d8b1e74fe8d6eb92e42f09f032138ead48bf66
Ajithkumar018ak <kathiresanajith018@gmail.com>
2026-06-18 17:23:01 +0530

    add

--- script.js
+++ script.js
@@ -629,3 +629,21 @@ window.onload = function(){
 };
 
 
+
+// Loader
+
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+        const loader = document.getElementById("loader");
+
+        loader.style.opacity = "0";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        },1000);
+
+    },2500);
+
+});
